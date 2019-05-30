e5871ed2aeae17eb32356dd51e7e649c3602a6eb
ForTheOtherss <qkrwotjd1445@naver.com>
2019-05-30 15:08:14 +0900

    웹툰 삭제하기(DELETE), 웹툰 에피소드 삭제(DELETE) 라우팅 완성

--- 황보소연,박재성_hw_6/routes/api/webtoons/episodes.js
+++ 황보소연,박재성_hw_6/routes/api/webtoons/episodes.js
@@ -80,4 +80,31 @@ router.get('/', async(req, res) => {
     }
 })
 
+router.delete('/', async(req, res) => {
+    try {
+        const { contentsIdx } = req.body;
+
+        var connection = await pool.getConnection();
+
+        // Params나 Body값 Null이 존재하는 경우
+        if (!contentsIdx) {
+            res.status(200).json(utils.successFalse(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
+        } else {
+            let query = 'DELETE FROM contents WHERE contentsIdx = ?';
+            let result = await connection.query(query, [contentsIdx]);
+            // Params에 잘못된 값이 입력된 경우
+            if (result.affectedRows == 0) {
+                res.status(200).json(utils.successFalse(statusCode.BAD_REQUEST, resMessage.WRONG_PARAMS));
+            } else {
+                res.status(200).json(utils.successTrue(statusCode.OK, resMessage.DELETE_SUCCESS));
+            }
+        }
+    } catch(err) {
+        console.log(err);
+        res.status(200).json(utils.successFalse(statusCode.INTERNAL_SERVER_ERROR, resMessage.INTERNAL_SERVER_ERROR));
+    } finally {
+        connection.release();
+    }
+});
+
 module.exports = router;
--- 황보소연,박재성_hw_6/routes/api/webtoons/webtoons.js
+++ 황보소연,박재성_hw_6/routes/api/webtoons/webtoons.js
@@ -81,4 +81,32 @@ router.get('/', async(req, res) => {
     }
 })
 
+router.delete('/', async(req, res) => {
+    try {
+        const { webtoonsIdx } = req.body;
+
+        var connection = await pool.getConnection();
+
+        // Params나 Body값 Null이 존재하는 경우
+        if (!webtoonsIdx) {
+            res.status(200).json(utils.successFalse(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
+        
+        } else {
+            let query = 'DELETE FROM webtoons WHERE webtoonsIdx = ?';
+            let result = await connection.query(query, [webtoonsIdx]);
+            // Params에 잘못된 값이 입력된 경우
+            if (result.affectedRows == 0) {
+                res.status(200).json(utils.successFalse(statusCode.BAD_REQUEST, resMessage.WRONG_PARAMS));
+            } else {
+                res.status(200).json(utils.successTrue(statusCode.OK, resMessage.DELETE_SUCCESS));
+            }
+        }
+    } catch(err) {
+        console.log(err);
+        res.status(200).json(utils.successFalse(statusCode.INTERNAL_SERVER_ERROR, resMessage.INTERNAL_SERVER_ERROR));
+    } finally {
+        connection.release();
+    }
+});
+
 module.exports = router;
